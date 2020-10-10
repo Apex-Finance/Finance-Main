dc2f91153a811bd20a1685a599c09efe13942527
Alexander-T-Mitchell <alexmitchell9715@gmail.com>
2020-10-10 11:57:00 -0600

    Basic creation of Budget, Transaction, and Category Model
    Made a simple Budget, Transaction, and Categroy model to use for the base UI

---
+++ Plutus/lib/models/budget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+import './transaction.dart';
+
+class Budget {
+  String id;
+  String title;
+  String category;
+  double amount;
+  List<Transaction> transactions;
+}
---
+++ Plutus/lib/models/category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class Category {
+  final String id;
+  final String title;
+
+  const Category({
+    @required this.id,
+    @required this.title,
+  });
+}
