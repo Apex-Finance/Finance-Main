fb621fe94a950b21139d847c1c5fa85b08b20d70
Alexander-T-Mitchell <alexmitchell9715@gmail.com>
2020-10-14 19:58:40 -0600

    Added dummy data
    Made some dummy data to be used. Changed the classes slightly

---
+++ Plutus/lib/data/budget_dummy_data.dart
@@ -0,0 +1,22 @@
+import '../models/budget.dart';
+
+const List<Budget> DUMMY_BUDGETS = const [
+  Budget(
+    id: 'b1',
+    title: 'Apparel',
+    category: 'c2',
+    amount: 200,
+  ),
+  Budget(
+    id: 'b2',
+    title: 'Luxuries',
+    category: 'c3',
+    amount: 100,
+  ),
+  Budget(
+    id: 'b3',
+    title: 'Necessities',
+    category: 'c1',
+    amount: 500,
+  ),
+];
