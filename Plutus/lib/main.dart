a661b866ff847fabc9f0199dabc6a32b3bc3c0ef
SevCass <jccass@icloud.com>
2020-11-02 20:51:49 -0600

    auth provider and screen created

--- Plutus/lib/main.dart
+++ Plutus/lib/main.dart
@@ -1,46 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
-import './screens/tab_screen.dart';
 import './screens/dashboard_screen.dart';
 import './screens/budget_screen.dart';
 import './screens/transaction_screen.dart';
 import './screens/goal_screen.dart';
+import './screens/auth_screen.dart';
+import './providers/auth.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Plutus',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        primaryColor: Colors.amber,
-        primaryColorLight: Colors.amberAccent,
-        accentColor: Colors.white,
-        canvasColor: Colors.black,
-        textTheme: GoogleFonts.latoTextTheme(
-          TextTheme(
-            bodyText1: TextStyle(
-              color: Colors.amber,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Auth(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Plutus',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          primaryColor: Colors.amber,
+          primaryColorLight: Colors.amberAccent,
+          accentColor: Colors.white,
+          canvasColor: Colors.black,
+          textTheme: GoogleFonts.latoTextTheme(
+            TextTheme(
+              bodyText1: TextStyle(
+                color: Colors.amber,
+              ),
             ),
           ),
         ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => OnBoardingPage(),
+          AuthScreen.routeName: (context) => AuthScreen(),
+          DashboardScreen.routeName: (context) => DashboardScreen(),
+          BudgetScreen.routeName: (context) => BudgetScreen(
+                budgets: [],
+              ),
+          TransactionScreen.routeName: (context) => TransactionScreen(
+                transactions: [],
+              ),
+          GoalScreen.routeName: (context) => GoalScreen(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => OnBoardingPage(),
-        DashboardScreen.routeName: (context) => DashboardScreen(),
-        BudgetScreen.routeName: (context) => BudgetScreen(
-              budgets: [],
-            ),
-        TransactionScreen.routeName: (context) => TransactionScreen(
-              transactions: [],
-            ),
-        GoalScreen.routeName: (context) => GoalScreen(),
-      },
     );
   }
 }
@@ -56,7 +66,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
 
   void _onIntroEnd(context) {
     Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) => TabScreen()),
+      MaterialPageRoute(builder: (_) => AuthScreen()),
     );
   }
 
