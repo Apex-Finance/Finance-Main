dca7e9c82a93274b7392d6f9e0f318c58d8cc9eb
SevCass <jccass@icloud.com>
2020-11-10 12:25:52 -0600

    Packages fixed

--- Plutus/lib/main.dart
+++ Plutus/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:Plutus/models/month_changer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 import './screens/tab_screen.dart';
 import './screens/intro_screen.dart';
