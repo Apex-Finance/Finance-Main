33ab6aa56f6b79db3f1123ba80f1913c5f30afb7
jsulse <juansulse@gmail.com>
2020-12-10 12:23:52 -0500

    Made the card bigger

--- Plutus/lib/widgets/goals_list_tile.dart
+++ Plutus/lib/widgets/goals_list_tile.dart
@@ -12,42 +12,54 @@ class _GoalsListTileState extends State<GoalsListTile> {
   DateTime _date = DateTime.now();
   @override
   Widget build(BuildContext context) {
-    return Card(
-      // TODO Round these edges
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
-        ),
-      ),
-      child: Container(
-        height: 100,
-        child: ListTile(
-          tileColor: Colors.grey[850],
-          leading: ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            // TODO this may need to be a constrained box
-            // max height and width should be 256 x 256 (icon dimensions)
-            child: Container(
-              // TODO Make this image icon bigger!
-              child: Image.network(
-                  'https://c-sf.smule.com/rs-s62/arr/03/75/7744782c-6b94-409d-93d0-61bda6d57ef9.jpg'),
-            ),
+    return Container(
+      height: 150,
+      child: Card(
+        // TODO Round these edges
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
           ),
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.all(5),
+          tileColor: Colors.grey[850],
+          leading: Image.network(
+            'https://2p2bboli8d61fqhjiqzb8p1a-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/1.jpg',
+            alignment: Alignment.center,
+          ),
+
+          // leading: ClipRRect(
+          //   borderRadius: BorderRadius.all(Radius.circular(20)),
+          //   // TODO this may need to be a constrained box
+          //   // max height and width should be 256 x 256 (icon dimensions)
+          //   child: ConstrainedBox(
+          //     constraints: BoxConstraints(
+          //       minWidth: 44,
+          //       minHeight: 44,
+          //       maxWidth: 70,
+          //       maxHeight: 70,
+          //     ),
+          //     // TODO Make this image icon bigger!
+          //     child: Image.network(
+          //         'https://cdn.carbuzz.com/gallery-images/840x560/243000/300/243339.jpg'),
+          //   ),
+          // ),
           title: Text(
             "Lambroghini Reventon",
-            style: Theme.of(context).textTheme.bodyText2,
+            style: Theme.of(context).textTheme.headline1,
           ),
           subtitle: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 "\$10,000",
-                style: Theme.of(context).textTheme.bodyText1,
+                style: Theme.of(context).textTheme.subtitle1,
               ),
               new LinearPercentIndicator(
                 padding: EdgeInsets.fromLTRB(0, 10, 5, 10),
                 alignment: MainAxisAlignment.center,
-                width: MediaQuery.of(context).size.width * .66,
+                width: MediaQuery.of(context).size.width * .6,
                 lineHeight: 12.0,
                 backgroundColor: Colors.black,
                 progressColor: Colors.amber,
@@ -60,7 +72,7 @@ class _GoalsListTileState extends State<GoalsListTile> {
                 children: <Widget>[
                   Text(
                     "${DateFormat.yMMMd().format(_date)}",
-                    style: Theme.of(context).textTheme.bodyText1,
+                    style: Theme.of(context).textTheme.bodyText2,
                   ),
                   Padding(
                     padding: EdgeInsets.only(
@@ -68,7 +80,7 @@ class _GoalsListTileState extends State<GoalsListTile> {
                     ),
                     child: Text(
                       "\$1,400,000",
-                      style: Theme.of(context).textTheme.bodyText1,
+                      style: Theme.of(context).textTheme.bodyText2,
                     ),
                   ),
                 ],
