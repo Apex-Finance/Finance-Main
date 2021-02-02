68df682e809a26eb6fa05d6326de7a96ddebd2ef
jsulse <juansulse@gmail.com>
2021-02-01 21:02:29 -0600

    Created goals class and continued work on goals screen UI

---
+++ Plutus/lib/models/goals.dart
@@ -0,0 +1,5 @@
+class Goals {
+  int id; // will be random (how? figure this out)
+  int title;
+  double amount;
+}
--- Plutus/lib/screens/goal_screen.dart
+++ Plutus/lib/screens/goal_screen.dart
@@ -7,33 +7,46 @@ class GoalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: Container(
-            width: 250,
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-            bottom: 20,
-          ),
-          child: Text(
-            "Goals & Wishes",
-            style: TextStyle(
-              color: Colors.amber,
-              fontSize: 30,
+    var goalID; // will change to reflect DB
+
+    return Container(
+      child: goalID == null
+          ? Center(
+              child: Text(
+                'No goals yet. Ready to add one?',
+                style: TextStyle(
+                    fontSize: 18, color: Theme.of(context).primaryColor),
+                textAlign: TextAlign.center,
+              ),
+            )
+          : Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Container(
+                    width: 250,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 20,
+                  ),
+                  child: Text(
+                    "Goals & Wishes",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                // Expanded(
+                //   child: ListView.builder(
+                //       shrinkWrap: true,
+                //       itemCount: 5,
+                //       itemBuilder: (context, index) => GoalsListTile()),
+                // ),
+              ],
             ),
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 5,
-              itemBuilder: (context, index) => GoalsListTile()),
-        ),
-      ],
     );
   }
 }
