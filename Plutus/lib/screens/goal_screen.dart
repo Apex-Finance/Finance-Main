82e437c25641dd63808afe9440c8a633f22e9d45
tzeus23 <tzurielazodoh23@gmail.com>
2021-02-04 10:38:50 -0600

    tabs

--- Plutus/lib/screens/goal_screen.dart
+++ Plutus/lib/screens/goal_screen.dart
@@ -12,6 +12,7 @@ class GoalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     Goal goal; // will change to reflect DB
     var dbRef = FirebaseFirestore.instance
         .collection('users')
@@ -21,34 +22,92 @@ class GoalScreen extends StatelessWidget {
         stream: dbRef.snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
               return Text('Loading...');
             default:
-              switch (snapshot.data.docs.isEmpty) {
+              /*switch (snapshot.data.docs.isEmpty) {
                 case true:
                   return Text('You have no goals yet...');
-                default:
-                  return ListView(
-                    padding: EdgeInsets.all(12.0),
-                    children: snapshot.data.docs.map((doc) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 5.0, vertical: 8.0),
-                        child: Container(
-                          child: ListTile(
-                            onLongPress: () {
-                              /*capture info from this document into the goal object provided above
-                            and offer them to either update or delete possibly*/
-                            },
+                default:*/
+              return Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    DefaultTabController(
+                      length: 2, // length of tabs
+                      //initialIndex: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Container(
+                            child: TabBar(
+                              indicatorColor: Theme.of(context).primaryColor,
+                              labelColor: Theme.of(context).primaryColor,
+                              unselectedLabelColor:
+                                  Theme.of(context).primaryColor,
+                              labelStyle: Theme.of(context).textTheme.headline1,
+                              tabs: [
+                                Tab(text: 'Goals'),
+                                Tab(text: 'Debt'),
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    }).toList(),
+                          Container(
+                            height: screenHeight * 0.73, //height of TabBarView
+                            child: TabBarView(
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: ListView.builder(
+                                          shrinkWrap: true,
+                                          itemCount: 5,
+                                          itemBuilder: (context, index) =>
+                                              GoalsListTile()),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: ListView.builder(
+                                          shrinkWrap: true,
+                                          itemCount: 5,
+                                          itemBuilder: (context, index) =>
+                                              GoalsListTile()),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            /*ListView(
+                padding: EdgeInsets.all(12.0),
+                children: snapshot.data.docs.map((doc) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 5.0, vertical: 8.0),
+                    child: Container(
+                      child: ListTile(
+                        onLongPress: () {
+                          capture info from this document into the goal object provided above
+                            and offer them to either update or delete possibly
+                        },
+                      ),
+                    ),
                   );
-              }
+                }).toList(),
+              );*/
           }
-        });
+        }
+        //}
+        );
   }
 }
