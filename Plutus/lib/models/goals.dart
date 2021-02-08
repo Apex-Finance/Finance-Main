dff10ef6199d2885a8b69b2330fb67fc3b729cc7
tzeus23 <tzurielazodoh23@gmail.com>
2021-02-08 16:10:58 -0600

    g

--- Plutus/lib/models/goals.dart
+++ Plutus/lib/models/goals.dart
@@ -5,71 +5,44 @@ import 'package:flutter/material.dart';
 import '../providers/auth.dart';
 
 class Goal {
-  String _id; // (db will generate primary key; just need to obtain it from db)
-  String _title; // Goal title
-  double _amountSaved; // Amount currently saved up
-  double _goalAmount; // Total amount of goal
-  DateTime _dateOfGoal; // Date goal was set
+  String id; // (db will generate primary key; just need to obtain it from db)
+  String title; // Goal title
+  double amountSaved; // Amount currently saved up
+  double goalAmount; // Total amount of goal
+  DateTime dateOfGoal; // Date goal was set
 
   // Call this function to set the data when you have gathered all the value for creating a goal
+  Goal({
+    this.title,
+    this.amountSaved,
+    this.goalAmount,
+    this.dateOfGoal,
+  });
 
   // will need to add databse code to obtain id from document
   void setID(String idValue) {
-    _id = idValue;
+    id = idValue;
   }
 
   String getID() {
-    return _id;
-  }
-
-  void setTitle(String titleValue) {
-    _title = titleValue;
+    return id;
   }
 
   String getTitle() {
-    return _title;
+    return title;
   }
 
-  void setAmountSaved(double amountValue) {
-    _amountSaved = amountValue;
-  }
-
-  double getAmountSaved() {
-    return _amountSaved;
-  }
-
-  void setGoalAmount(double amountValue) {
-    _goalAmount = amountValue;
+  double getAmount() {
+    return amountSaved;
   }
 
   double getGoalAmount() {
-    return _goalAmount;
-  }
-
-  void setDate(DateTime dateValue) {
-    _dateOfGoal = dateValue;
-  }
-
-  DateTime getDate() {
-    return _dateOfGoal;
+    return goalAmount;
   }
 }
 
 // This class will be handling all database code; it will essentially be acting like the provider we used for budgets
 class GoalDataProvider with ChangeNotifier {
-  // Initialize a goal with the document data
-  Goal initializeGoal(DocumentSnapshot doc) {
-    Goal goal;
-
-    goal.setID(doc.id);
-    goal.setAmountSaved(doc.data()['amountSaved']);
-    goal.setTitle(doc.data()['title']);
-    goal.setGoalAmount(doc.data()['goalAmount']);
-    goal.setDate(doc.data()['date']);
-
-    return goal;
-  }
-
   void addGoal(Goal goal, BuildContext context) async {
     await FirebaseFirestore.instance
         .collection('users')
@@ -78,9 +51,8 @@ class GoalDataProvider with ChangeNotifier {
         .doc()
         .set({
       'title': goal.getTitle(),
-      'amountSaved': goal.getAmountSaved(),
+      'amount': goal.getAmount(),
       'goalAmount': goal.getGoalAmount(),
-      'date': goal.getDate(),
     });
   }
 
@@ -93,7 +65,7 @@ class GoalDataProvider with ChangeNotifier {
         .set(
       {
         'title': goal.getTitle(),
-        'amount': goal.getAmountSaved(),
+        'amount': goal.getAmount(),
         'goalAmount': goal.getGoalAmount(),
       },
       SetOptions(merge: true),
