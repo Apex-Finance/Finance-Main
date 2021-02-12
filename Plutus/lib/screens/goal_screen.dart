c7c742bc7b3db707dc3944768e8c0c33ce1b5582
Josiah Cassada <jccass@icloud.com>
2021-02-12 15:42:05 -0600

    Revert "Merge branch 'Tzuriel's-branch' into Testing----11/20-12/10"
    This reverts commit 073621a0e273b5d3ba2cdc32949f7d6df843adcb, reversing
    changes made to 8b352cf69ca3f54b235399be21096721675bdb4e.

--- Plutus/lib/screens/goal_screen.dart
+++ Plutus/lib/screens/goal_screen.dart
@@ -1,97 +1,136 @@
 import 'package:flutter/material.dart';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
-import '../widgets/goals_form.dart';
-import '../widgets/goals_list_tile.dart';
-import '../models/goals.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../providers/auth.dart';
+import '../models/goals.dart';
+import '../widgets/goals_list_tile.dart';
 
 class GoalScreen extends StatelessWidget {
   static const routeName = '/goal';
-  void _enterGoal(BuildContext context) {
-    showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (_) => GoalsForm(),
-    ).then((newGoal) {
-      if (newGoal == null) return;
-      Provider.of<GoalDataProvider>(context, listen: false)
-          .addGoal(newGoal, context); //TODO check if needed
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    GoalDataProvider goalDataProvider;
-
-    Goal goal;
+    double screenHeight = MediaQuery.of(context).size.height;
+    Goal goal; // will change to reflect DB
+    var goalDataProvider =
+        Provider.of<GoalDataProvider>(context, listen: false);
+    var authDataProvider = Provider.of<Auth>(context);
     var dbRef = FirebaseFirestore.instance
         .collection('users')
-        .doc(Provider.of<Auth>(context, listen: false).getUserId())
+        .doc(authDataProvider.getUserId())
         .collection('Goals');
     return StreamBuilder<QuerySnapshot>(
-      stream: dbRef.snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return Center(
-              child: Text(
-                'Loading...',
-                style: TextStyle(
-                    fontSize: 18, color: Theme.of(context).primaryColor),
-                textAlign: TextAlign.center,
-              ),
-            );
-          default:
-            switch (snapshot.data.docs.isEmpty) {
-              case true:
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'You have no goals yet.',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Theme.of(context).primaryColor),
-                        textAlign: TextAlign.center,
+        stream: dbRef.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Loading...');
+            default:
+              /*switch (snapshot.data.docs.isEmpty) {
+                case true:
+                  return Text('You have no goals yet...');
+                default:*/
+              return Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    DefaultTabController(
+                      length: 2, // length of tabs
+                      //initialIndex: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Container(
+                            child: TabBar(
+                              indicatorColor: Theme.of(context).primaryColor,
+                              labelColor: Theme.of(context).primaryColor,
+                              unselectedLabelColor:
+                                  Theme.of(context).primaryColor,
+                              labelStyle: Theme.of(context).textTheme.headline1,
+                              tabs: [
+                                Tab(text: 'Goals'),
+                                Tab(text: 'Debt'),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: screenHeight * 0.73, //height of TabBarView
+                            child: TabBarView(
+                              children: <Widget>[
+                                Card(
+                                  color: Colors.grey[900],
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.vertical(
+                                      top: Radius.circular(20),
+                                    ),
+                                  ),
+                                  child: Expanded(
+                                    child: ListView.builder(
+                                      //shrinkWrap: true,
+                                      itemCount: snapshot.data.docs.length,
+                                      itemBuilder: (context, index) {
+                                        snapshot.data.docs.map(
+                                          (doc) {
+                                            // initialize the goal with the document data
+                                            goal = goalDataProvider
+                                                .initializeGoal(doc);
+                                          },
+                                        );
+                                        return GoalsListTile(goal);
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                //TODO build Debt list tile
+                                Column(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: ListView.builder(
+                                        shrinkWrap: true,
+                                        itemCount: snapshot.data.docs.length,
+                                        itemBuilder: (context, index) {
+                                          snapshot.data.docs.map(
+                                            (doc) {
+                                              // initialize the goal with the document data
+                                              goal = goalDataProvider
+                                                  .initializeGoal(doc);
+                                            },
+                                          );
+                                          return GoalsListTile(goal);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      RaisedButton(
-                        child: Text('Add Goal'),
-                        color: Theme.of(context).primaryColor,
-                        textColor: Theme.of(context).canvasColor,
-                        onPressed: () => _enterGoal(context),
+                    ),
+                  ],
+                ),
+              );
+            /*ListView(
+                padding: EdgeInsets.all(12.0),
+                children: snapshot.data.docs.map((doc) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 5.0, vertical: 8.0),
+                    child: Container(
+                      child: ListTile(
+                        onLongPress: () {
+                          capture info from this document into the goal object provided above
+                            and offer them to either update or delete possibly
+                        },
                       ),
-                    ],
-                  ),
-                );
-              default:
-                return Card(
-                  color: Colors.grey[900],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(20),
                     ),
-                  ),
-                  child: Expanded(
-                    child: ListView.builder(
-                      itemCount: snapshot.data.docs.length + 1,
-                      itemBuilder: (context, index) {
-                        snapshot.data.docs.map(
-                          (doc) {
-                            goal = goalDataProvider.initializeGoal(doc);
-                          },
-                        );
-                        return GoalsListTile(goal);
-                      },
-                    ),
-                  ),
-                );
-            }
+                  );
+                }).toList(),
+              );*/
+          }
         }
-      },
-    );
+        //}
+        );
   }
 }
