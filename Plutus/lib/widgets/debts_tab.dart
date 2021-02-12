057fe43eb6938c51fd72af1bd692f40900c286c3
Josiah Cassada <jccass@icloud.com>
2021-02-12 15:33:23 -0600

    Revert "Merge branch 'Tzuriel's-branch' into Testing----11/20-12/10"
    This reverts commit 073621a0e273b5d3ba2cdc32949f7d6df843adcb, reversing
    changes made to 8b352cf69ca3f54b235399be21096721675bdb4e.

---
+++ Plutus/lib/widgets/debts_tab.dart
@@ -0,0 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../widgets/goals_list_tile.dart';
+import '../models/goals.dart';
+import '../providers/auth.dart';
+
+class DebtTab extends StatefulWidget {
+  @override
+  _DebtTabState createState() => _DebtTabState();
+}
+
+class _DebtTabState extends State<DebtTab> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    GoalDataProvider goalDataProvider;
+
+    Goal goal;
+    var dbRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc(Provider.of<Auth>(context, listen: false).getUserId())
+        .collection('Goals');
+    return StreamBuilder<QuerySnapshot>(
+      stream: dbRef.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Text('Loading...');
+          default:
+            switch (snapshot.data.docs.isEmpty) {
+              case true:
+                return Text('You have no goals yet...');
+              default:
+                return Card(
+                  color: Colors.grey[900],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                  ),
+                  child: Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data.docs.length,
+                      itemBuilder: (context, index) {
+                        snapshot.data.docs.map(
+                          (doc) {
+                            goal = goalDataProvider.initializeGoal(doc);
+                          },
+                        );
+                        return GoalsListTile(goal);
+                      },
+                    ),
+                  ),
+                );
+            }
+        }
+      },
+    );
+  }
+}
