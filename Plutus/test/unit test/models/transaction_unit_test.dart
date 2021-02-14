e14d97862c6dd95bf8165310c9f5a98d78c5700f
Josiah Cassada <jccass@icloud.com>
2021-02-13 20:19:48 -0600

    Update transaction_unit_test.dart

--- Plutus/test/unit test/models/transaction_unit_test.dart
+++ Plutus/test/unit test/models/transaction_unit_test.dart
@@ -1,16 +1,51 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:Plutus/models/transaction.dart';
-import 'package:Plutus/models/categories.dart';
 
 void main() {
+  Transaction ogTransaction;
+  Transaction newTransaction;
   group('initializeTransaction', () {
     test('Test string comparison', () {
-      Transaction ogTransaction{
-        ogTransaction.id = '',
-        ogTransaction.title = 'title', 
-        ogTransaction.date = getDate();
-      }
-    })
-  });
+      ogTransaction.setID('1');
+      ogTransaction.setTitle('Jedi');
+      ogTransaction.setCategoryId('entertainment');
+      ogTransaction.setAmount(10000.00);
+      ogTransaction.setDate(DateTime.now());
 
+      newTransaction.setID('1');
+      newTransaction.setTitle('Jedi');
+      newTransaction.setCategoryId('entertainment');
+      newTransaction.setAmount(10000.00);
+      newTransaction.setDate(DateTime.now());
+
+      expect(ogTransaction.getID(), newTransaction.getID());
+      expect(ogTransaction.getTitle(), newTransaction.getTitle());
+      expect(ogTransaction.getCategoryId(), newTransaction.getCategoryId());
+      expect(ogTransaction.getAmount(), newTransaction.getAmount());
+      expect(ogTransaction.getDate(), newTransaction.getDate());
+    });
+
+    test('Test Transaction for null', () {
+      expect(ogTransaction.getID(), isNot(null));
+      expect(newTransaction.getID(), isNot(null));
+    });
+  });
+  group('getTransactionExpenses', (){
+    test('Test accurate totals', (){
+      double testTotalExpenses = 0;
+
+      Transaction t1, t2, t3, t4;
+
+      t1.setAmount(1.00);
+      t2.setAmount(5.00);
+      t3.setAmount(10.00);
+      t4.setAmount(20.00);
+
+      testTotalExpenses = t1.getAmount() + t2.getAmount()  + t3.getAmount() + t4.getAmount();
+
+      expect(testTotalExpenses, 36.00);
+      expect(testTotalExpenses, isNot(0.00));
+
+    })
+  })
 }
