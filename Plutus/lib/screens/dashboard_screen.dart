0dffbee4b78cdca1f668e2fca64bb7ab85a656b5
tzeus23 <tzurielazodoh23@gmail.com>
2021-03-14 13:08:54 -0500

    chart install

--- Plutus/lib/screens/dashboard_screen.dart
+++ Plutus/lib/screens/dashboard_screen.dart
@@ -1,15 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class DashboardScreen extends StatelessWidget {
   static const routeName = '/dashboard';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Dashboard Screen',
-        style: Theme.of(context).textTheme.bodyText1,
-      ),
+    return Column(
+      children: [
+        Card(
+            color: Colors.grey[850],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'graphical overview of budget',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ],
+            )),
+        Card(
+          color: Colors.grey[850],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            children: [
+              Text(
+                'recent transactions',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
+        ),
+        Card(
+          color: Colors.grey[850],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+          ),
+          child: Text(
+            'upcoming goals',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ),
+        Card(
+          color: Colors.grey[850],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+          ),
+          child: Text(
+            'faq',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ),
+      ],
     );
   }
 }
