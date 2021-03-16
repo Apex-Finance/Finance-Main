d7e2aedf7bd44bc44d2d52f951f8de35bca93a7f
TurtleTech8 <58091353+TurtleTech8@users.noreply.github.com>
2021-03-16 10:38:50 -0500

    Caused it to update value

--- Plutus/lib/screens/new_budget_screens/first_budget_screen.dart
+++ Plutus/lib/screens/new_budget_screens/first_budget_screen.dart
@@ -20,24 +20,25 @@ class FirstBudgetScreen extends StatefulWidget {
 }
 
 var categoryList = new List<Category>();
-double remainingAmount;
+List<FocusNode> catAmountFocusNodes = List<FocusNode>();
 
 class _FirstBudgetScreenState extends State<FirstBudgetScreen> {
   double activeAmount = 0;
 
   // Sets the category and amount for the current ListTile being built
-  void calculateAmountLeft() {
+  void calculateAmountLeft(int index) {
     double amount = 0.00;
     categoryList.forEach((category) {
       amount += category.getAmount();
     });
-    widget.budget.categoryAmount = amount;
+    setState(() {
+      widget.budget.categoryAmount = amount;
+    });
     return;
   }
 
   @override
   Widget build(BuildContext context) {
-    remainingAmount = widget.budget.getAmount();
     var categoryDataProvider = Provider.of<CategoryDataProvider>(context);
     var category = Category();
 
@@ -116,9 +117,8 @@ class _FirstBudgetScreenState extends State<FirstBudgetScreen> {
                               categoryDataProvider.initializeCategory(doc));
                         });
                         categoryList = tempList;
-                        List<FocusNode> catAmountFocusNodes =
-                            List<FocusNode>.generate(
-                                categoryList.length, (index) => FocusNode());
+                        catAmountFocusNodes = List<FocusNode>.generate(
+                            categoryList.length, (index) => FocusNode());
                         if (widget.budget.getID() != null) {
                           return StreamBuilder<QuerySnapshot>(
                             stream: BudgetDataProvider().getBudgetCategories(
