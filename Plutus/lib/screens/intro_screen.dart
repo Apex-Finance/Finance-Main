ac2ed7b75e379cf690a5eedb47e80906c861f5e7
jsulse <juansulse@gmail.com>
2021-03-22 21:09:36 -0500

    Removed some uneeded files; added orderby to sort goal list tiles; added vectors in replacement for old pics in intro screen; corrected some grammar; added some documentation

--- Plutus/lib/screens/intro_screen.dart
+++ Plutus/lib/screens/intro_screen.dart
@@ -3,6 +3,7 @@ import 'package:introduction_screen/introduction_screen.dart';
 
 import './auth_screen.dart';
 
+// First screens the user sees upon installing the app. Offers a concise overview of what Plutus has to offer
 class OnBoardingPage extends StatefulWidget {
   @override
   _OnBoardingPageState createState() => _OnBoardingPageState();
@@ -16,7 +17,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       child: Image.asset(
         'images/$assetName',
         height: 300,
-        width: 350.0,
+        width: 350,
         fit: BoxFit.cover,
       ),
       alignment: Alignment.bottomCenter,
@@ -34,7 +35,8 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
           fontSize: 28.0, fontWeight: FontWeight.w700, color: Colors.amber),
       bodyTextStyle: bodyStyle,
       descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-      pageColor: Colors.black,
+      // Default app theme is white, hence white background in intro screen
+      pageColor: Colors.white,
       imagePadding: EdgeInsets.zero,
     );
 
@@ -44,29 +46,35 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
         PageViewModel(
           title: "Welcome to Plutus",
           body: "Plutus helps you take control of your finances with ease.",
-          image: _buildImage('83667a8005313ca195808d9262b91586.jpg'),
+          image: _buildImage('Welcome.jpg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Create a Budget",
           body:
               "Whether you're planning out next month's expenses or thinking about your next vacation, Plutus helps all your budgeting needs.",
-          image: _buildImage('budget-image.jpg'),
+          image: _buildImage('Budget.jpg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Manage Transactions",
           body:
-              "Stay on top of your expenses with Plutus. Know exactly what and when you spent your money.",
-          image: _buildImage(
-              'Keep-Track-of-the-Financial-Transactions-Within-Komet_1080x.png'),
+              "Stay on top of your expenses with Plutus. Know exactly what you spent your money on and when you spent it.",
+          image: _buildImage('Transaction.jpg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "Set a goal",
           body:
               "Saving up for that shiny new car? Plutus can help you set aside funds for that item and keep track of how close you are to achieving that goal.",
-          image: _buildImage('financial-goals-icons-600w-1118707967.jpg'),
+          image: _buildImage('Goal.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Utilize the Quick Add Button",
+          body:
+              "Use the + button to quickly add budgets, transactions, and goals. Just tap on the corresponding icon.",
+          image: _buildImage('Quick Add Button.jpg'),
           decoration: pageDecoration,
         ),
         PageViewModel(
@@ -80,29 +88,10 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
           image: _buildImage('lets get started.jpg'),
           decoration: pageDecoration,
         ),
-        // NOT NEEDED IN INITIAL WALKTHROUGH
-        // PageViewModel(
-        //   title: "Want to manually create a budget?",
-        //   bodyWidget: RaisedButton(
-        //     onPressed: () {
-        //       Navigator.of(context).popAndPushNamed(IncomeScreen.routeName);
-        //     },
-        //     child: const Text(
-        //       'Create a Budget',
-        //       style: TextStyle(color: Colors.black),
-        //     ),
-        //     color: Colors.amber,
-        //     shape: RoundedRectangleBorder(
-        //       borderRadius: BorderRadius.circular(8.0),
-        //     ),
-        //   ),
-        //   image: _buildImage('Budgeting Methods_Banner.png'),
-        //   decoration: pageDecoration,
-        // ),
       ],
       onDone: () => Navigator.of(context).pushNamed(AuthScreen.routeName),
       onSkip: () => introKey.currentState?.animateScroll(
-          4), //_onIntroEnd(context), // You can override onSkip callback
+          5), //_onIntroEnd(context), // You can override onSkip callback
       showSkipButton: true,
       skipFlex: 0,
       nextFlex: 0,
@@ -110,7 +99,10 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
         'Skip',
         style: TextStyle(color: Colors.amber),
       ),
-      next: const Icon(Icons.arrow_forward),
+      next: const Icon(
+        Icons.arrow_forward,
+        color: Colors.amber,
+      ),
       done: const Text('Done',
           style: TextStyle(
             fontWeight: FontWeight.w600,
