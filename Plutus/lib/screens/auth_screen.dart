9490ccaf6c9c4152a01be49003c408c453fc3878
jsulse <juansulse@gmail.com>
2021-03-25 13:43:22 -0500

    Fix issue with Login where keyboard covers the LOGIN button

--- Plutus/lib/screens/auth_screen.dart
+++ Plutus/lib/screens/auth_screen.dart
@@ -29,51 +29,49 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ),
-          SingleChildScrollView(
-            child: Container(
-              height: deviceSize.height,
-              width: deviceSize.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Flexible(
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
-                      transform: Matrix4.rotationZ(0) // -8 * pi / 180
-                        ..translate(0.0),
-                      // ..translate(-10.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
-                          )
-                        ],
-                      ),
-                      child: Text(
-                        'Plutus login',
-                        style: TextStyle(
-                          color:
-                              Theme.of(context).accentTextTheme.headline6.color,
-                          fontSize: 25,
-                          fontFamily: 'Anton',
-                          fontWeight: FontWeight.normal,
-                        ),
+          Container(
+            height: deviceSize.height,
+            width: deviceSize.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 20.0),
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
+                    transform: Matrix4.rotationZ(0) // -8 * pi / 180
+                      ..translate(0.0),
+                    // ..translate(-10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                        )
+                      ],
+                    ),
+                    child: Text(
+                      'Plutus login',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).accentTextTheme.headline6.color,
+                        fontSize: 25,
+                        fontFamily: 'Anton',
+                        fontWeight: FontWeight.normal,
                       ),
                     ),
                   ),
-                  Flexible(
-                    flex: deviceSize.width > 600 ? 2 : 1,
-                    child: AuthCard(),
-                  ),
-                ],
-              ),
+                ),
+                Flexible(
+                  flex: deviceSize.height > 600 ? 3 : 2,
+                  child: AuthCard(),
+                ),
+              ],
             ),
           ),
         ],
--- Plutus/lib/widgets/auth_form.dart
+++ Plutus/lib/widgets/auth_form.dart
@@ -110,108 +110,102 @@ class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
-    return KeyboardAvoider(
-      //autoScroll: true,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        elevation: 8.0,
-        child: Container(
-          height: _authMode == AuthMode.Signup ? 320 : 260,
-          constraints: BoxConstraints(
-              minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 8.0,
+      child: Container(
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'E-Mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                  controller: _emailController,
+                  validator: (value) {
+                    if (value.isEmpty || !value.contains('@')) {
+                      return 'Invalid email!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    email = value.trim();
+                  },
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  onEditingComplete: () => {
+                    if (_authMode == AuthMode.Signup)
+                      {FocusScope.of(context).nextFocus()}
+                    else
+                      {FocusScope.of(context).unfocus()}
+                  },
+                  obscureText: true,
+                  controller: _passwordController,
+                  // ignore: missing_return
+                  validator: (value) {
+                    if (value.isEmpty || value.trim().length < 6) {
+                      return 'Password is too short!';
+                    }
+                  },
+                  onSaved: (value) {
+                    password = value.trim();
+                  },
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+                if (_authMode == AuthMode.Signup)
                   TextFormField(
-                    decoration: InputDecoration(labelText: 'E-Mail'),
-                    keyboardType: TextInputType.emailAddress,
-                    onEditingComplete: () => FocusScope.of(context).nextFocus(),
-                    controller: _emailController,
-                    validator: (value) {
-                      if (value.isEmpty || !value.contains('@')) {
-                        return 'Invalid email!';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      email = value.trim();
-                    },
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Password'),
-                    onEditingComplete: () => {
-                      if (_authMode == AuthMode.Signup)
-                        {FocusScope.of(context).nextFocus()}
-                      else
-                        {FocusScope.of(context).unfocus()}
-                    },
+                    enabled: _authMode == AuthMode.Signup,
+                    decoration: InputDecoration(labelText: 'Confirm Password'),
+                    onEditingComplete: () => FocusScope.of(context).unfocus(),
                     obscureText: true,
-                    controller: _passwordController,
-                    // ignore: missing_return
-                    validator: (value) {
-                      if (value.isEmpty || value.trim().length < 6) {
-                        return 'Password is too short!';
-                      }
-                    },
-                    onSaved: (value) {
-                      password = value.trim();
-                    },
+                    validator: _authMode == AuthMode.Signup
+                        // ignore: missing_return
+                        ? (value) {
+                            if (value != _passwordController.text.trim()) {
+                              return 'Passwords do not match!';
+                            }
+                          }
+                        : null,
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
-                  if (_authMode == AuthMode.Signup)
-                    TextFormField(
-                      enabled: _authMode == AuthMode.Signup,
-                      decoration:
-                          InputDecoration(labelText: 'Confirm Password'),
-                      onEditingComplete: () => FocusScope.of(context).unfocus(),
-                      obscureText: true,
-                      validator: _authMode == AuthMode.Signup
-                          // ignore: missing_return
-                          ? (value) {
-                              if (value != _passwordController.text.trim()) {
-                                return 'Passwords do not match!';
-                              }
-                            }
-                          : null,
-                      style: Theme.of(context).textTheme.bodyText2,
+                SizedBox(
+                  height: 20,
+                ),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  RaisedButton(
+                    child:
+                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                    onPressed: _submit,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  if (_isLoading)
-                    CircularProgressIndicator()
-                  else
-                    RaisedButton(
-                      child: Text(
-                          _authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-                      onPressed: _submit,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                      color: Theme.of(context).primaryColor,
-                      textColor:
-                          Theme.of(context).primaryTextTheme.button.color,
-                    ),
-                  FlatButton(
-                    child: Text(
-                        '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-                    onPressed: _switchAuthMode,
                     padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    textColor: Theme.of(context).primaryColor,
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).primaryTextTheme.button.color,
                   ),
-                ],
-              ),
+                FlatButton(
+                  child: Text(
+                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                  onPressed: _switchAuthMode,
+                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  textColor: Theme.of(context).primaryColor,
+                ),
+              ],
             ),
           ),
         ),
