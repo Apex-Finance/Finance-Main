7b69a7030d56cd05632df646ec2943ef3bc0d989
tzeus23 <tzurielazodoh23@gmail.com>
2021-03-27 15:33:49 -0500

    manual revert

--- Plutus/lib/screens/dashboard_screen.dart
+++ Plutus/lib/screens/dashboard_screen.dart
@@ -1,71 +1,170 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:Plutus/models/charts.dart';
-
-import 'package:provider/provider.dart';
-import '../providers/auth.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import '../models/pie_chart.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   static const routeName = '/dashboard';
 
   @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  List<charts.Series<PiePiece, String>> _pieSeriesData;
+  _generateData() {
+    var pieData = [
+      new PiePiece('home', 35.8, Colors.blue),
+      new PiePiece('food_and_drinks', 8.3, Colors.red),
+      new PiePiece('bills', 10.8, Colors.green),
+      new PiePiece('eduction', 15.6, Colors.yellow),
+      new PiePiece('shopping', 19.2, Colors.orange),
+      new PiePiece('travel', 10.3, Colors.pink),
+      new PiePiece('entertainment', 12.4, Colors.purple),
+      new PiePiece('investments', 15.7, Colors.brown),
+      new PiePiece('uncategorized', 24.8, Colors.black),
+    ];
+
+    _pieSeriesData.add(
+      charts.Series(
+        domainFn: (PiePiece piePiece, _) => piePiece.category,
+        measureFn: (PiePiece piePiece, _) => piePiece.amount,
+        colorFn: (PiePiece piePiece, _) =>
+            charts.ColorUtil.fromDartColor(piePiece.colorVal),
+        id: 'How you spent',
+        data: pieData,
+        labelAccessorFn: (PiePiece row, _) => '${row.category}',
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pieSeriesData = List<charts.Series<PiePiece, String>>();
+    _generateData();
+  }
+
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 30.0),
-      child: Expanded(
-        child: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: GroupedBarChart.withSampleData(),
+    return Column(
+      children: [
+        Card(
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Container(
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Dashboard chart',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    SizedBox(height: 10),
+                    Expanded(
+                      child: charts.PieChart(
+                        _pieSeriesData,
+                        animate: true,
+                        animationDuration: Duration(seconds: 2),
+                        behaviors: [
+                          new charts.DatumLegend(
+                            outsideJustification:
+                                charts.OutsideJustification.endDrawArea,
+                            horizontalFirst: false,
+                            desiredMaxRows: 2,
+                            cellPadding:
+                                new EdgeInsets.only(right: 4, bottom: 4),
+                            entryTextStyle: charts.TextStyleSpec(
+                                color:
+                                    charts.MaterialPalette.purple.shadeDefault,
+                                fontFamily: 'Georgia',
+                                fontSize: 11),
+                          )
+                        ],
+                        defaultRenderer: new charts.ArcRendererConfig(
+                          arcWidth: 100,
+                          arcRendererDecorators: [
+                            new charts.ArcLabelDecorator(
+                                labelPosition: charts.ArcLabelPosition.inside)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Divider(
-              color: Theme.of(context).primaryColor,
+          ),
+        ),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                'Dashboard chart',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
+        ),
+        Row(
+          children: [
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    'Dashboard chart',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ],
+              ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(
-                          Provider.of<Auth>(context, listen: false).getUserId())
-                      .collection('budgets')
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData || snapshot.data.docs.isEmpty) {
-                      return Text("No Data Available");
-                    }
-                    List<QueryDocumentSnapshot> data = snapshot.data.docs;
-                    List<TimeSeriesSales> budgetGiven = List<TimeSeriesSales>();
-                    List<charts.Series<TimeSeriesSales, DateTime>> chartData =
-                        List<charts.Series<TimeSeriesSales, DateTime>>();
-                    for (QueryDocumentSnapshot doc in data) {
-                      budgetGiven.add(TimeSeriesSales(
-                          DateTime.parse(doc["title"]), doc["amount"].round()));
-                    }
-                    chartData.add(
-                      charts.Series<TimeSeriesSales, DateTime>(
-                        id: 'Budget',
-                        colorFn: (_, __) =>
-                            charts.ColorUtil.fromDartColor(Colors.blueAccent),
-                        domainFn: (TimeSeriesSales sales, _) => sales.time,
-                        measureFn: (TimeSeriesSales sales, _) => sales.sales,
-                        data: budgetGiven,
-                      ),
-                    );
-                    return NormalTimeChart(chartData, animate: false);
-                    // return NormalTimeChart(chartData, animate: false);
-                  },
-                ),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    'Dashboard chart',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ],
               ),
             ),
           ],
         ),
-      ),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                'Dashboard chart',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
+        ),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                'Dashboard chart',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
+        ),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                'Dashboard chart',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
