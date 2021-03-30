451d48ef93d92ad73d80f1a95b36792c66f6f69e
Josiah Cassada <jccass@icloud.com>
2021-03-29 21:35:59 -0500

    Revert "Revert "Merge branch 'Testing----3/29-3/12' into Juan--Fix-Goal-Screen-Bugs""
    This reverts commit 6b2043940a2678f91d23196b71d2970ed030f6e9.

--- Plutus/lib/screens/auth_screen.dart
+++ Plutus/lib/screens/auth_screen.dart
@@ -1,26 +1,25 @@
-// import 'dart:math';
+import 'package:flutter/material.dart';
 
 import '../widgets/auth_form.dart';
 
-import 'package:flutter/material.dart';
-
+// Login Screen
 class AuthScreen extends StatelessWidget {
   static const routeName = '/auth';
 
   @override
   Widget build(BuildContext context) {
+    // Adjusts based on the size of the device
     final deviceSize = MediaQuery.of(context).size;
-    // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
-    // transformConfig.translate(-10.0);
+
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
       body: Stack(
         children: <Widget>[
+          // Background gradient
           Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [
-                  Colors.black,
+                  Colors.white,
                   Colors.amber,
                 ],
                 begin: Alignment.topLeft,
@@ -29,72 +28,23 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ),
-          SingleChildScrollView(
-            child: Container(
-              height: deviceSize.height,
-              width: deviceSize.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Flexible(
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
-                      transform: Matrix4.rotationZ(0) // -8 * pi / 180
-                        ..translate(0.0),
-                      // ..translate(-10.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
-                          )
-                        ],
-                      ),
-                      child: Text(
-                        'Plutus login',
-                        style: TextStyle(
-                          color:
-                              Theme.of(context).accentTextTheme.headline6.color,
-                          fontSize: 25,
-                          fontFamily: 'Anton',
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    flex: deviceSize.width > 600 ? 2 : 1,
-                    child: AuthCard(),
-                  ),
-                ],
-              ),
+          // White card containing login interactables
+          Container(
+            height: deviceSize.height,
+            width: deviceSize.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  flex: deviceSize.height > 600 ? 3 : 2,
+                  child: AuthForm(),
+                ),
+              ],
             ),
           ),
         ],
       ),
     );
-  }
-}
-
-class AuthCard extends StatefulWidget {
-  const AuthCard({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _AuthCardState createState() => _AuthCardState();
-}
-
-//TODO connect this widget with its builder method
-class _AuthCardState extends State<AuthCard> {
-  @override
-  Widget build(BuildContext context) {
-    return AuthForm();
   }
 }
